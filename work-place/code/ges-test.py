597d4e9312d90f236a909a921ba7f3f709e13f7e
Sudipto <princesudipto2002@gmail.com>
2022-11-22 21:00:59 +0530

    git-test change

--- work-place/code/ges-test.py
+++ work-place/code/ges-test.py
@@ -1,21 +1,78 @@
+#importing required libraries
+
 from math import sqrt
 from tkinter import SOLID
 import math
 import cv2
 import mediapipe as mp
 import numpy as np
+import os, sys
+import mysql.connector as msql
+
+
+#Extracting hand recognition solution
 
 mpHands = mp.solutions.hands
-hands = mpHands.Hands(max_num_hands=1) 
+hands = mpHands.Hands(max_num_hands=1)
+
+
+#Assigning a variable to video capturing
 
 cap = cv2.VideoCapture(0)
 
+
+#extract unique serial number of system
+
+os_t = sys.platform.lower()
+if "win" in os_t:
+    command = "wmic bios get serialnumber"
+elif "linux" in os_t:
+    command = "hal-get-property --udi /org/freedesktop/Hal/devices/computer --key system.hardware.uuid"
+elif "darwin" in os_t:
+    command = "ioreg -l | grep IOPlatformSerialNumber"
+sn = os.popen(command).read().replace("\n","").replace("	","").replace(" ","").replace("SerialNumber",'')
+sn = sn.lower()
+
+
+#stablishing connection with database
+
+conn = msql.connect(
+  host="localhost",
+  user="root",
+  password="123456789"
+)
+
+
+#checking whether the required database is in the server
+
+mycursor = conn.cursor()
+if conn.is_connected():
+    mycursor.execute("SHOW DATABASES")
+    for x in mycursor:
+        if sn in x:
+            flag = 1
+            break
+        else:
+            flag = 0
+    print(flag)
+else:
+    print("Connection not stablished")
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+#Measurement prediction
+
 X = [26, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49, 51, 53, 55, 57, 59, 61, 63]    #measured length
 Y = [161, 150, 139, 128, 118, 110, 104, 100, 94, 90, 84, 81, 79, 75, 73, 70, 66, 64, 62, 62]    #shown length between index 5 and 17
-Z = [423, 392, 365, 348, 332, 310, 291, 274, 255, 243, 230, 224, 211, 205, 195, 188, 178, 170, 163, 161]    ##shown length between index 0 and 12
+Z = [423, 392, 365, 348, 332, 310, 291, 274, 255, 243, 230, 224, 211, 205, 195, 188, 178, 170, 163, 161]    #shown length between index 0 and 12
+
+
+#Cofficient calculation
 
 coff = np.polyfit(Y, X, 2)
-coff2 = np.polyfit(Z, X, 2)
+coff2 = np.polyfit(Z, Y, 2)
+
 A, B, C = coff
 D, E, F = coff2
 
@@ -23,28 +80,37 @@ A = (A+D)/2
 B = (B+E)/2
 C = (C+F)/2
 
+
 while True:
-    success, img = cap.read()
-    img = cv2.flip(img, 90)
+    success, img = cap.read()   #img capture
+    img = cv2.flip(img, 90)     #flipping the image
     
-    if not success: 
+    if not success:            #condition if the image is not capturing
         break
     
-    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)      #converting image format from BGR to RGB
     results = hands.process(imgRGB)
 
-    h, w, c = img.shape
+    h, w, c = img.shape         #extracting image size
     
-    if results.multi_hand_landmarks:
-        x_arr = [21]
-        y_arr = [21]
-        dis_arr = [21]
-        for handLms in results.multi_hand_landmarks:
+    if results.multi_hand_landmarks:        #condition if the palm detected
+        x_arr = []        #it store x coordinates of 21 index points
+        y_arr = []        #it store y coordinates of 21 index points
+        dis_arr = []          #it store distance between required 21 index points
+
+        for handLms in results.multi_hand_landmarks:        #accessing the coordinated of landmarks
+
+
+            #marking the index points on palm
+
             for id, lm in enumerate(handLms.landmark):
                 cx, cy = int(lm.x*w), int(lm.y*h)
                 cv2.circle(img, (int(cx), int(cy)), 3, (10,10,10), cv2.FILLED)
                 x_arr = x_arr[:id]+[cx]+x_arr[id+1:]
                 y_arr = y_arr[:id]+[cy]+y_arr[id+1:]
+
+
+            #joining the required index points and finding the distance between two index points
 
             for id, lm in enumerate(handLms.landmark):
                 if id == 0:
@@ -162,19 +228,18 @@ while True:
                     dis = math.trunc(sqrt(pow((x_arr[19] - x_arr[20]),2) + pow((y_arr[19] - y_arr[20]),2)))
                     dis = A * dis**2 + B*dis + C
                     dis_arr = dis_arr[:19]+[dis]+x_arr[19+1:]
-            
-            print(dis_arr,"\n")
 
-            dis = math.trunc(sqrt(pow((x_arr[5] - x_arr[17]),2) + pow((y_arr[5] - y_arr[17]),2)))
-            dis = str(A * dis**2 + B*dis + C)
-            text = "Length between index 05 & 17 = "
-            text = text + dis
-            cv2.putText(img, text, (5, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2, cv2.LINE_AA)
-            '''dis = math.trunc(sqrt(pow((x_arr[12] - x_arr[0]),2) + pow((y_arr[12] - y_arr[0]),2)))
-            dis = str(A * dis**2 + B*dis + C)
-            text = "Length between index 0 & 12 = "
-            text = text + dis
-            cv2.putText(img, text, (5, 55), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2, cv2.LINE_AA)'''
+
+        #data fenching and matching from database
+
+        #if required database not exist in the server
+        if flag == 0:
+            mycursor.execute("CREATE DATABASE %s" %sn)
+            cv2.putText(img, 'This system is new.', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
+        
+        #if required database exist in the server
+        else:
+            cv2.putText(img, 'HI.', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
 
     cv2.imshow("Gesture Security and Control System", img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
